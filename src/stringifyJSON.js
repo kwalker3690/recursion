af9364556234591af60b46eb39b8ee21100635e2
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-06-27 11:07:53 -0700

    Initial commit, one test case still failing

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,66 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+
+  if (typeof obj != "object"){
+    var result;
+
+    if(typeof obj === "string"){
+      result = '"' + String(obj) + '"'
+      return result;
+    }else if(typeof obj === "number" || typeof obj === "boolean"){
+      return String(obj)
+    }
+    else if (typeof obj === "null"){
+      return null
+    }
+    else{
+      result = '"' + String(obj) + '"'
+      return result;
+    }
+  }
+  else {
+    var arrayType = (Object.prototype.toString.call(obj) === '[object Array]');
+    var objectType = (Object.prototype.toString.call(obj) === '[object Object]');
+    var newArray = []
+
+    for(item in obj){
+      var value = obj[item];
+      var type = typeof value;
+
+      if(Object.prototype.toString.call(value) === '[object Object]'){
+        newArray.push('"' + item + '"' + ':' + stringifyJSON(value))
+      }
+      else if (Object.prototype.toString.call(value) === '[object Array]'){
+        newArray.push('"' + item + '"' + ':' + stringifyJSON(value))
+      }
+      else{
+        value = stringifyJSON(value);
+
+        if(arrayType){
+          newArray.push(value)
+        }
+        else{
+          newArray.push('"' + item + '"' + ':' + value)
+        }
+      }
+    }
+
+    if(arrayType){
+      return "[" + newArray + "]";
+    }
+    else if(objectType) {
+      return "{" + newArray + "}"
+    }
+    else{
+      return 'null';
+    }
+  }
 };
+
+// base case : if object is json, return
+
+// action: convert value to string
+
+// recursive case: based on what segments have to be converted to string (ie object keys etc)
